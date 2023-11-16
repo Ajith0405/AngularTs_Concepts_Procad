32228bee79a4698bab432aba0798b510625c89c1
AJITH S <128063551+Ajith0405@users.noreply.github.com>
2023-11-16 16:51:49 +0530

    first commit

---
+++ Input_output_decorators/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProductListComponent } from './Container/product-list/product-list.component';
+import { SearchComponent } from './Container/search/search.component';
+import { ProductComponent } from './Container/product/product.component';
+import { FilterComponent } from './Container/product-list/filter/filter.component';
+
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductListComponent,
+    SearchComponent,
+    ProductComponent,
+    FilterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
